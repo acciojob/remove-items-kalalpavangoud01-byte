b12734f24fc8d92ff906fb7bb79ebe57feee6c38
kalalpavangoud01-byte <kalalpavangoud01@gmail.com>
2026-09-19 16:21:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+const colorSelect = document.getElementById("colorSelect");
+const removeBtn = document.querySelector('input[type="button"]');
+
+removeBtn.addEventListener("click", () => {
+    colorSelect.remove(colorSelect.selectedIndex);
+});
